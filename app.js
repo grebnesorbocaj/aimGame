829a2f867b953196a41bed82da2646f48d8fef07
unknown <bocajgrebnesor@gmail.com>
2019-09-26 22:46:52 -0400

    made some changes

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
+var scoreBoard = document.getElementById('scoreboard')
+
 var difficulty = 10;
 var numTargets = 100;
+var numLives = 5;
+
+if(window.innerWidth < 800){
+  var maxRadius = 60;
+  var radChange = 0.9;
+}else {
+  var maxRadius = 75;
+  var radChange = 0.4;
+}
 
 function Circle(x, y, radius, radChange){
   this.x = x;
@@ -44,6 +55,10 @@ function Circle(x, y, radius, radChange){
         this.radChange = 0;
         this.ignore = true;
         if(this.clicked == false) {
+          document.getElementById('redCover').style.display = 'block'
+          setTimer(function(){
+            document.getElementById('redCover').style.display = 'none'
+          }, 70)
           missedCount += 1;
         }
       }
@@ -70,9 +85,6 @@ var mouse = {
 
 
 
-
-
-
 function animate() {
   requestAnimationFrame(animate); //requests to repeat animate function
   c.clearRect(0,0,innerWidth,innerHeight); // clear frame so that new frame can be drawn
@@ -81,7 +93,7 @@ function animate() {
     mouse.y = e.y;
     mouse.clicked = true;
   })
-  if((pointCount + missedCount) == numTargets){
+  if((pointCount + missedCount) == numTargets ||  numLives == missedCount){
     loadAgainOr()
     cancelAnimationFrame(animate)
   }else{
@@ -91,7 +103,6 @@ function animate() {
       }else{
         if(Math.floor(Math.random() * 15) == 0){
           let radius = 25;
-          let radChange = 0.9;
           let x = Math.random() * (innerWidth - radius * 2) + radius;
           let y = Math.random() * (innerHeight - radius * 2) + radius;
           circleArray.push(new Circle(x,y,radius,radChange));
@@ -103,6 +114,7 @@ function animate() {
       circleArray[jol].update()
     }
     clickAccuracy = pointCount / (pointCount + clickCount + missedCount) * 100 || 0;
+    scoreBoard.innerHTML = `${pointCount} points from ${clickCount} clicks. Missed ${missedCount} circles, ${numLives-missedCount} lives left.`
   }
   if(mouse.clicked == true){
     clickCount += 1;
